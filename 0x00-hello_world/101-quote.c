153a302f335dbfaeea5cbc47a60883ffdbf1f440
Tebyansalh <tebyansalah1@gmail.com>
2023-08-10 19:34:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: 1 (error).
  */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(1, quo, 59);
+	fput( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
 	return (1);
 }
