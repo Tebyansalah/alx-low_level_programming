307d29a4c608077d3692d61d3b90feb5d7bb36fe
Tebyansalh <tebyansalah1@gmail.com>
2023-08-10 19:21:20 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 1 (not Success)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
